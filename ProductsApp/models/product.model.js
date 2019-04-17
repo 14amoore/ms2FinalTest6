46afaba0c1ec59579eced8d38ef2a5badcd904da
Adam Moore <adamoore@gmail.com>
2019-04-17 14:55:42 -0400

    all functions working

--- ProductsApp/models/product.model.js
+++ ProductsApp/models/product.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  name: [{type: String, max: 100}],
+  name: {type: String, max: 100},
   price: {price: Number},
 });
 
--- app.js
+++ app.js
@@ -6,6 +6,17 @@ const product = require('./ProductsApp/routes/product.route');
 // initialize the express app
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const dev_db_url = 'mongodb+srv://amuser:098765@cluster0-dhtes.mongodb.net/test?retryWrites=true';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
 const port = 8000;
@@ -13,12 +24,3 @@ const port = 8000;
 app.listen(port, () => {
   console.log('Server is up and running on port ' + port);
 });
-
-// Set up mongoose connection
-const mongoose = require('mongoose');
-let dev_db_url = 'https://cloud.mongodb.com/v2/5cb628d2c56c982fe1b18d78#access';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
